490136d8e2e71f31fa3d7654f0200e3c67a61c6a
Jonas Liao <jonhinric2331@hotmail.com>
2017-05-21 06:07:12 +0000

    Integrated Redux

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import React, {Component} from 'react'
+// import { render } from 'react-dom'
+// import { createStore } from 'redux'
+// import { Provider } from 'react-redux'
+// import reducer from './reducers'
+//
+// const store = createStore(reducer)
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <h1>Hello heact :)</h1>
+//     );
+//   }
+// }
+// export default App;

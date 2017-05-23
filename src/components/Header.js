1595dd1d051b4eb4f41da328e22e705bacce3129
peachy19 <prasg19@gmail.com>
2017-05-23 20:34:26 +0000

    Cleaned up code to start the base code

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Header = () => (
+  <p>
+
+  </p>
+)
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-// Application entrypoint.
-// Load up the application styles
-import { BrowserRouter } from 'react-router-dom'
 require('../styles/application.scss');
 
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 import App from './components/App'
-import reducer from './reducers'
-
-const store = createStore(reducer)
 
 render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
+      <App />,
   document.getElementById('react-root')
 )

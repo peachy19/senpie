71299ffe61f5fba2184c835ef0137adb2bc217d2
Prachi <prachi@bacon.local>
2017-05-27 12:12:06 -0700

    Renamed files

--- src/reducers/searchbar.js
+++ src/reducers/searchbar.js
@@ -1,9 +1,12 @@
 import { UPDATE_SEARCHBAR } from '../actionTypes';
 //Searchbar text is updated from blank to user query
-export const searchbarText = (state='', action)=> {
+export const searchbar = (state={}, action)=> {
   switch (action.type) {
     case UPDATE_SEARCHBAR:{
-      return action.payload;
+      return {
+        text: action.payload,
+        style: action.style
+      }
     }
   }
   return state;

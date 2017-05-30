373496d0f70e78eddb8339fe62e91fa73c5858a8
peachy <prasg19@gmail.com>
2017-05-30 07:23:35 -0700

    Displayed distance b/w user's location and results

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,38 +7,62 @@ class Map extends Component {
 
   componentDidMount () {
     const map = new google.maps.Map(this.refs.map, {zoom: 11});
-    const points = [];
 
-    this.props.mentors.map((mentor) => {
-      const loc = {
-        lat: Number(mentor[0].latitude), lng: Number(mentor[0].longitude), data: {name: mentor[0].user_name, job_title: mentor[0].job_title}
-      }
-      points.push(loc);
-    });
+    const infoWindow = new google.maps.InfoWindow({ content: ''});
 
-    console.log('Points are', points);
+    let points = [];
+    let userLocation = {};
+    const mentors = this.props.mentors;
 
-    // var points = [
-    //   {lat: 49.23232, lng: -123.1003, label: 'a', data: {name:'john', age:33, city:'vancouver'}},
-    //   {lat: 49.27232, lng: -123.1193, label: 'b'}
-    // ];
-
-
-
-    const infoWindow = new google.maps.InfoWindow({
-      content: ''
-    });
-
+    console.log('1. DOM loaded');
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
+        userLocation = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-        infoWindow.setPosition(pos);
+
+        console.log('2.Received userLocation ', userLocation);
+        infoWindow.setPosition(userLocation);
         infoWindow.setContent('Your location');
         infoWindow.open(map);
-        map.setCenter(pos);
+        map.setCenter(userLocation);
+
+        mentors.map((mentor) => {
+          console.log('3.getMentors, userLocation ', userLocation);
+          const p1 = new google.maps.LatLng(Number(mentor[0].latitude), Number(mentor[0].longitude));
+          console.log("p2", userLocation);
+          const p2 = new google.maps.LatLng(userLocation.lat, userLocation.lng);
+
+          const dist = (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
+          console.log("Distance", dist);
+          const loc = {
+            lat: Number(mentor[0].latitude), lng: Number(mentor[0].longitude), data: {name: mentor[0].user_name, job_title: mentor[0].job_title, distance: dist}
+          }
+          points.push(loc);
+        });
+
+      console.log('Points are', points);
+
+        points.map(function(p){
+
+        var marker = new google.maps.Marker({
+          position: p,
+          label: p.label,
+          animation: google.maps.Animation.DROP,
+          map: map,
+          data: p.data
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          //this has the point and its a marker
+          infoWindow.setContent('<div style="color:blue">'+this.data.name+'<br>'+this.data.job_title+'<br>Distance: '+this.data.distance+'</div>');
+          infoWindow.open(map, this);
+        });
+      });
+
+    map.setCenter(points[0]);
+
       }, function() {
         infoWindow.setPosition(map.getCenter());
         infoWindow.setContent(true ?
@@ -46,26 +70,8 @@ class Map extends Component {
           'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
       });
+
     }
-
-    points.map(function(p){
-
-      var marker = new google.maps.Marker({
-        position: p,
-        label: p.label,
-        animation: google.maps.Animation.DROP,
-        map: map,
-        data: p.data
-      });
-
-      google.maps.event.addListener(marker, 'click', function() {
-        //this has the point and its a marker
-        infoWindow.setContent('<div style="color:blue">'+this.data.name+'<br>'+this.data.job_title+'</div>');
-        infoWindow.open(map, this);
-      });
-    });
-
-    map.setCenter(points[0]);
   }
 
   render() {

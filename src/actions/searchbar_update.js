3d76c7d461b2a8e2ff6c38535f2c922d67d50def
peachy <prasg19@gmail.com>
2017-05-31 08:28:20 -0700

    Fixed searchbar bugs

--- src/actions/searchbar_update.js
+++ src/actions/searchbar_update.js
@@ -1,7 +1,6 @@
 import { UPDATE_SEARCHBAR } from '../actionTypes';
 
-export const updateSearchbar = (query, style) => ({
+export const updateSearchbar = (query) => ({
   type: UPDATE_SEARCHBAR,
-  payload: query,
-  style: style
+  query
 })
--- src/reducers/searchbar.js
+++ src/reducers/searchbar.js
@@ -2,10 +2,10 @@ import { UPDATE_SEARCHBAR } from '../actionTypes';
 //Searchbar text is updated from blank to user query
 export const searchbar = (state={}, action)=> {
   switch (action.type) {
-    case UPDATE_SEARCHBAR:{
+    case UPDATE_SEARCHBAR: {
+      const { query } = action;
       return {
-        text: action.payload,
-        style: action.style
+        text: query
       }
     }
   }
